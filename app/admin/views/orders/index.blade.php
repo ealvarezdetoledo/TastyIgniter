bdb725164b9b36c837593d61bcd6bba512ef35e9
ealvarezdetoledo <56493928+ealvarezdetoledo@users.noreply.github.com>
2021-01-31 22:36:11 +0100

    Update index.blade.php
    Included auto-refresher

--- app/admin/views/orders/index.blade.php
+++ app/admin/views/orders/index.blade.php
@@ -1,4 +1,23 @@
+
+<script type="text/JavaScript">
+function timedRefresh(timeoutPeriod) {
+var timer = setInterval(function() {
+if (timeoutPeriod > 1) {
+timeoutPeriod -= 1;
+document.getElementById("countdown").innerHTML = timeoutPeriod;
+} else {
+clearInterval(timer);
+location.reload();
+};
+}, 1000);
+};
+timedRefresh(31);
+</script>
+
+
+
 <div class="row-fluid">
+    <div style="text-align: right; margin-bottom: -5rem; padding-top: 1.5rem;"><h6>Auto refresh in <span id="countdown"></span> seconds&nbsp;</h6><br></div>
     {!! $this->renderList() !!}
 </div>
 
